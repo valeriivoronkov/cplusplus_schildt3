555572f191f4b755c50c0618da52ee8cef6880dc
voronkovvaleriy <voronglass84@gmail.com>
2018-11-05 19:51:25 +0300

    Add example 2 page22

---
+++ page22/page22.cpp
@@ -0,0 +1,26 @@
+/*
+Программа №2 - Использование переменной.
+ */
+
+/* 
+ * File:   page22.cpp
+ * Author: xcoder
+ *
+ * Created on November 5, 2018, 6:37 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+int x;	// Здесь объявляется переменная.
+ x = 1023;	// Здесь переменной х присваивается число 1023.
+ cout << "Эта программа выводит значение переменной х: ";
+ cout << x; // Отображение числа 1023.
+    return 0;
+}
+
