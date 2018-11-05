a464a094f64cfa305565d3a1cd0c8692aaef3e7f
voronkovvaleriy <voronglass84@gmail.com>
2018-11-05 18:25:42 +0300

    Add first example page16

---
+++ page16/page16.cpp
@@ -0,0 +1,23 @@
+/* Программа № - Первая С++-программа.
+Введите эту программу, затем скомпилируйте ее и выполните.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: xcoder
+ *
+ * Created on November 5, 2018, 1:58 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+cout << "Это моя первая С++-программа.";
+    return 0;
+}
+
