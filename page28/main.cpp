f4dc662da4634c2520a3e3b1dc5c0a41568c9777
voronkovvaleriy <voronglass84@gmail.com>
2019-01-12 16:02:17 +0300

    Add example 6 page 28
    -- function arguments

---
+++ page28/main.cpp
@@ -0,0 +1,23 @@
+// Использование функции abs().
+
+/* 
+ * File:   main.cpp
+ * Author: xcoder
+ *
+ * Created on January 12, 2019, 3:47 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <valarray>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << abs(-12.4);
+    return 0;
+}
+
