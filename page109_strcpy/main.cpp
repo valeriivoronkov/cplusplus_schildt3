8a6470a86bcb8db30d38cb1d56327fe6eaf4a8dd
voronkovvaleriy <voronglass84@gmail.com>
2019-01-13 17:56:26 +0300

    Add example 12 page 109
    -- example of strcpy

---
+++ page109_strcpy/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.cpp
+ * Author: xcoder
+ *
+ * Created on January 13, 2019, 5:47 PM
+ */
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char str[80];
+    strcpy(str, "Just forgot to save changes");
+    cout << str;
+    return 0;
+}
+
